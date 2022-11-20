95cfcf7359101b9728cdbd19cce477275ad9a727
Mitchell Horne <mhorne@FreeBSD.org>
2022-11-20 17:05:50 -0400

    hwpmc: pcpu_fini cleanup

--- sys/dev/hwpmc/hwpmc_mod.c
+++ sys/dev/hwpmc/hwpmc_mod.c
@@ -5732,19 +5732,23 @@ pmc_cleanup(void)
 	maxcpu = pmc_cpu_max();
 
 	PMCDBG0(MOD,INI,3, "md cleanup");
-	if (md) {
+	if (md != NULL) {
+		/*
+		 * Per-class PCPU finalization. This is the analogue to
+		 * pmc_smp_init().
+		 */
 		pmc_save_cpu_binding(&pb);
 		for (cpu = 0; cpu < maxcpu; cpu++) {
-			PMCDBG2(MOD,INI,1,"pmc-cleanup cpu=%d pcs=%p",
-			    cpu, pmc_pcpu[cpu]);
-			if (!pmc_cpu_is_active(cpu) || pmc_pcpu[cpu] == NULL)
+			if (!pmc_cpu_is_active(cpu))
 				continue;
 			pmc_select_cpu(cpu);
-			for (c = 0; c < md->pmd_nclass; c++)
+			for (c = 0; c < md->pmd_nclass; c++) {
 				if (md->pmd_classdep[c].pcd_num > 0)
 					md->pmd_classdep[c].pcd_pcpu_fini(md,
 					    cpu);
+			}
 		}
+		pmc_restore_cpu_binding(&pb);
 
 		if (md->pmd_cputype == PMC_CPU_GENERIC)
 			pmc_generic_cpu_finalize(md);
@@ -5753,7 +5757,6 @@ pmc_cleanup(void)
 
 		pmc_mdep_free(md);
 		md = NULL;
-		pmc_restore_cpu_binding(&pb);
 	}
 
 	/* Free per-cpu descriptors. */
