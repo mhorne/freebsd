424cbb1b36bb3743b7c91e66f5c811b722511814
Mitchell Horne <mhorne@FreeBSD.org>
2023-06-22 17:06:10 -0300

    hwpmc: tidy pcd_finalize methods
    Make them a little bit more consistent with one another in terms of what
    is done:
     - Add/reduce assertions to what is most useful: a loop to ensure
       pcpu_fini freed everything
     - Add PMCDBG trace entries
    
    The exception is the dmc620/cmn600 classes which behave a little
    differently, so leave them untouched.

--- sys/dev/hwpmc/hwpmc_core.c
+++ sys/dev/hwpmc/hwpmc_core.c
@@ -1262,6 +1262,10 @@ pmc_core_finalize(struct pmc_mdep *md)
 {
 	PMCDBG0(MDP,INI,1, "core-finalize");
 
+	for (int i = 0; i < pmc_cpu_max(); i++)
+		KASSERT(core_pcpu[i] == NULL,
+		    ("[core,%d] non-null pcpu cpu %d", __LINE__, i));
+
 	free(core_pcpu, M_PMC);
 	core_pcpu = NULL;
 }
